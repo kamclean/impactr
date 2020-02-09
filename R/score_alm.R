d2fc6093008f7821ce47b537bec4f5f4bfcf5ea6
kamclean <mcleankaca@gmail.com>
2020-02-09 18:52:07 +0000

    .

--- R/score_alm.R
+++ R/score_alm.R
@@ -5,8 +5,11 @@
 #' @param doi Vector of unique Digital Object Identifiers (DOI)
 #' @return Vector of overall almetric scores (or NA if not available)
 #' @import dplyr
+#' @import tibble
 #' @importFrom rAltmetric altmetric_data altmetrics
 #' @export
 
 # Function-------------------------------
-score_alm <- function(doi){unlist(lapply(doi, function(x){tryCatch(rAltmetric::altmetric_data(rAltmetric::altmetrics(doi = x))$score, error=function(e) NA)}))}
+score_alm <- function(doi){
+  lapply(doi, function(x){tryCatch(rAltmetric::altmetric_data(rAltmetric::altmetrics(doi = x))$score, error=function(e){NA})}) %>%
+    unlist()}
