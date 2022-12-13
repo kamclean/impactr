7fc849d5063bf393bb99afa50e850b8814c648a6
kamclean <mcleankaca@gmail.com>
2022-12-13 18:35:50 +0000

    update almetric functions

--- R/impact_altmetric.R
+++ R/impact_altmetric.R
@@ -8,7 +8,7 @@
 #' @import dplyr
 #' @import tibble
 #' @import stringr
-#' @importFrom rAltmetric altmetric_data altmetrics
+#' @import httr
 #' @importFrom purrr map_df
 #' @importFrom tidyr unite pivot_longer pivot_wider unnest
 #' @importFrom lubridate as_date as_datetime
@@ -17,119 +17,47 @@
 # Function-------------------------------
 # aim to be able to use either pmid or doi
 impact_altmetric <- function(id_list){
-  # https://api.altmetric.com/docs/call_citations.html
-  # https://github.com/ropensci/rAltmetric/issues/27
+
 
   # Set-up--------------------------
   # Load packages
-  require(dplyr);require(tibble);require(purrr);require(tidyr)
-  require(lubridate);require(magrittr);require(rAltmetric);require(stringr)
-
-  # Define essential columnns
-  var <- c("doi", "title", "type", "author_list", "journal", "issns", "altmetric_id",
-           "history.1w","history.1m","history.3m","history.6m", "history.1y",
-           "score","context.all.mean", "context.all.count", "context.all.rank",
-           "context.journal.mean","context.journal.rank","context.journal.count",
-           "context.similar_age_journal_3m.mean","context.similar_age_journal_3m.rank", "context.similar_age_journal_3m.count")
-
-  var_dates <- c("published_on", "last_updated","added_on")
-
-  essential_col = c("authors", var, var_dates)
-
-  essential_df <- tibble::enframe(essential_col, name = "value", value = "name") %>%
-    dplyr::mutate(value = NA_character_) %>%
-    tidyr::pivot_wider()
 
   # Extract data
-
-  id_class <- as.character(id_list) %>%
-    tibble::enframe(name = "n", value = "id") %>%
-    # https://www.crossref.org/blog/dois-and-matching-regular-expressions/
-    dplyr::mutate(id_type = dplyr::case_when(stringr::str_detect(id, "^10\\.\\d{4,9}/")==T ~  "doi",
-                                             nchar(id)==8&is.numeric(as.numeric(id))==T ~ "pmid",
-                                             TRUE ~ "invalid"))
+  id_class <- suppressWarnings(as.character(id_list) %>%
+                                 tibble::enframe(name = "n", value = "id") %>%
+                                 # https://www.crossref.org/blog/dois-and-matching-regular-expressions/
+                                 dplyr::mutate(id_type = dplyr::case_when(stringr::str_detect(id, "^10\\.\\d{4,9}/")==T ~  "doi",
+                                                                          nchar(id)==8&is.numeric(as.numeric(id))==T ~ "pmid",
+                                                                          TRUE ~ "invalid")))
 
   df <- id_class %>%
     dplyr::filter(id_type %in% c("pmid", "doi")) %>%
-    dplyr::group_split(id) %>%
-    purrr::map_df(function(x){
+    dplyr::mutate(request = paste0("https://api.altmetric.com/v1/", id_type, "/",id)) %>%
+    dplyr::mutate(response = purrr::map(request, function(x){httr::GET(x)}),
+                  response404 = purrr::map_chr(response, function(x){httr::status_code(x) == 404})) %>%
+    dplyr::filter(response404==FALSE) %>%
+    dplyr::select(-request, -response404,-n,-id_type) %>%
+    dplyr::mutate(purrr::map_df(response, function(x){
+      jsonlite::fromJSON(httr::content(x, as = "text"), flatten = TRUE) %>%
+        rlist::list.flatten() %>%
+        tibble::enframe() %>%
+        tidyr::pivot_wider(everything())})) %>%
+    dplyr::select(-response) %>%
+    dplyr::full_join(id_class %>% select(id), by = "id") %>%
+    dplyr::mutate(journal_issn= paste0(issns, collapse = ", "),
+                  author_list= paste0(authors, collapse = ", ")) %>%
+    dplyr::select(-schema,-is_oa, -starts_with("publisher_subjects"),
+                  -issns, -authors, -starts_with("cohorts.")) %>%
+    dplyr::mutate(across(everything(), function(x){as.character(x)})) %>%
+    tidyr::unnest(everything()) %>%
+    dplyr::mutate(across(everything(), function(x){ifelse(x=="NULL", NA, x)})) %>%
+    dplyr::mutate(across(c(starts_with("context."),starts_with("cited_by"),starts_with("history."), "score"),
+                         function(x){as.numeric(x)})) %>%
+    dplyr::mutate(across(any_of(c("last_updated", "published_on", "added_on", "pubdate", "epubdate")),
+                         function(x){lubridate::as_date(lubridate::as_datetime(as.numeric(x)))})) %>%
+    dplyr::rename("date_update" = last_updated, "date_publish" = published_on, "date_added" = added_on,
+                  "date_publish_print" = pubdate, "date_publish_online" ="epubdate") %>%
 
-      data <- tryCatch(rAltmetric::altmetrics(pmid = if(x$id_type=="pmid"){x$id}else{NULL},
-                                              doi = if(x$id_type=="doi"){x$id}else{NULL}) %>%
-                           rAltmetric::altmetric_data() %>% tibble::as_tibble(),
-                         error=function(e) essential_df)
-
-      for(i in 1:length(essential_col)){
-        if(essential_col[i] %in% names(data)){data <- data}else{
-          data <- data %>%
-            dplyr::mutate(new_col = NA) %>%
-            dplyr::rename_at(vars(matches("new_col")), function(x){x = essential_col[i]})}}
-
-      data <- data %>%
-        tidyr::unite(col = "author_list",tidyselect::starts_with("authors"), sep = ", ", remove = TRUE) %>%
-        tidyr::unite(col = "journal_issn", tidyselect::starts_with("issns"), sep = ", ", remove = TRUE)})
-
-  # Altmetric sources--------------
-  sources_old <- c("posts", "accounts","tweeters","fbwalls",
-                   "msm","policies","peer_review_sites", "wikipedia",
-                   "feeds", "forum", "gplus","rh", "linkedin", "readers",
-                   "delicious","pinners","qs","weibo", "rdts","videos")
-
-  names_cited_old <- paste0("cited_by_",sources_old,"_count")
-
-  names_cited <- tibble::tibble(old = names_cited_old,
-                                new = sources_old) %>%
-    dplyr::mutate(new = dplyr::case_when(new == "fbwalls" ~ "fb",
-                                         new =="posts" ~ "all",
-                                         new =="tweeters" ~ "twitter_posts",
-                                         new =="accounts" ~ "twitter_accounts",
-                                         new =="msm" ~ "news_media",
-                                         new =="wikipedia" ~ "wikipedia",
-                                         new =="feeds" ~ "blogs",
-                                         new =="forum" ~ "forum",
-                                         new =="gplus" ~ "googleplus",
-                                         new =="msm" ~ "news",
-                                         new =="linkedin" ~ "linkedin",
-                                         new =="rh" ~ "research_highlight",
-                                         new =="policies" ~ "policy_source",
-                                         new =="delicious" ~ "other_1",
-                                         new =="pinners" ~ "other_2",
-                                         new =="qs" ~ "other_3",
-                                         new =="weibo" ~ "other_4",
-                                         new =="rdts" ~ "other_5",
-                                         new =="videos" ~ "other_6",
-                                         TRUE ~ new)) %>%
-    dplyr::filter(! new %in% c("delicious", "pinners", "qs","weibo")) %>%
-    dplyr::mutate(new = paste0("n_engage_", new))
-
-  names_cited_included <- names_cited %>% dplyr::filter(old %in% names(df))
-
-  for(i in 1:nrow(names_cited_included)){
-    df <- df %>%
-      dplyr::rename_at(vars(matches(names_cited_included$old[i])),
-                       function(x){x = names_cited_included$new[i]})}
-
-  names_cited_excluded <- names_cited %>%
-    dplyr::filter(! old %in% names(df)) %>%
-    dplyr::filter(! new %in% names(df)) %>%
-    dplyr::mutate(value = list(rep(NA, nrow(df)))) %>%
-    tidyr::pivot_wider(id_cols=-old, names_from = new) %>%
-    tidyr::unnest(col = everything())
-
-  df <- dplyr::bind_cols(df, names_cited_excluded) %>%
-    dplyr::mutate_at(vars(starts_with("n_engage_")), function(x){ifelse(is.na(x)==T, 0, x) %>% as.numeric()}) %>%
-    dplyr::mutate(n_engage_other =  dplyr::select(., starts_with("n_engage_other")) %>% rowSums(na.rm = T)) %>%
-    dplyr::select(-starts_with("n_engage_other_"))
-
-  # Altmetric dates--------------
-  df <- df %>%
-    dplyr::mutate(last_updated = lubridate::as_date(lubridate::as_datetime(as.numeric(last_updated))),
-                  published_on = lubridate::as_date(lubridate::as_datetime(as.numeric(published_on))),
-                  added_on = lubridate::as_date(lubridate::as_datetime(as.numeric(added_on)))) %>%
-    dplyr::rename("date_update" = last_updated, "date_publish" = published_on, "date_added" = added_on)
-
-  # Output --------------
-  df_out <- df %>%
     dplyr::rename_at(vars(starts_with("history.")),
                      function(x){stringr::str_replace(x, "history.", "alm_score_")}) %>%
     dplyr::rename("alm_score_now" = alm_score_at) %>%
@@ -141,23 +69,49 @@ impact_altmetric <- function(id_list){
                      function(x){stringr::str_replace(x, "context.similar_age_3m.", "alm_3m_")}) %>%
     dplyr::rename_at(vars(starts_with("context.all.")),
                      function(x){stringr::str_replace(x, "context.all.", "alm_all_")}) %>%
-    dplyr::rename_at(vars(ends_with("_count")),
-                     function(x){stringr::str_replace(x, "_count", "_n")}) %>%
-    dplyr::mutate_at(vars(tidyselect::starts_with("alm_")), as.numeric) %>%
+    dplyr::rename_at(vars(ends_with("_count")&starts_with("alm_")),
+                     function(x){stringr::str_replace(x, "_count", "_n")})
 
-    dplyr::mutate(alm_all_prop = 1-((alm_all_rank-1)/alm_all_n),
-                  alm_3m_prop = 1-((alm_3m_rank-1)/alm_3m_n),
-                  alm_journal_all_prop = 1-((alm_journal_all_rank-1)/alm_journal_all_n),
-                  alm_journal_3m_prop = 1-((alm_journal_3m_rank-1)/alm_journal_3m_n)) %>%
-    dplyr::mutate(id = id_class %>% dplyr::filter(id_type %in% c("pmid", "doi")) %>% dplyr::pull(id)) %>%
-    dplyr::select(id, doi,altmetric_id,title,
-                  type, author_list, journal, journal_issn,
-                  starts_with("alm_score_"),
-                  starts_with("alm_all_"), starts_with("alm_journal_all_"),
-                  starts_with("alm_3m_"), starts_with("alm_journal_3m_"),
-                  starts_with("date_"), starts_with("n_engage_"), "url_image" = images.large)
+  # Altmetric sources--------------
+  sources <- df %>%
+    dplyr::select(id, starts_with("cited_by_"), starts_with("readers")) %>%
+    dplyr::mutate(across(-id, function(x){as.numeric(x)})) %>%
+    tidyr::pivot_longer(cols = -id) %>%
+    dplyr::mutate(type = ifelse(stringr::str_detect(name, "readers")==T, "reader", "post"),
+                  name = stringr::str_remove_all(name, "_count|cited_by_|readers."),
+                  name = case_when(name =="accounts" ~ "online_users",
+                                   name == "fbwalls" ~ "fb",
+                                   name =="accounts" ~ "all",
+                                   name =="tweeters" ~ "twitter_users",
+                                   name =="posts" ~ "twitter_tweets",
+                                   name =="msm" ~ "news_media",
+                                   name =="wikipedia" ~ "wikipedia",
+                                   name =="feeds" ~ "blogs",
+                                   name =="forum" ~ "forum",
+                                   name =="gplus" ~ "googleplus",
+                                   name =="msm" ~ "news",
+                                   name =="linkedin" ~ "linkedin",
+                                   name =="rh" ~ "research_highlight",
+                                   name =="policies" ~ "policy_source",
+                                   TRUE ~ name)) %>%
+    dplyr::mutate(value = ifelse(is.na(value)==T, 0, value)) %>%
+    dplyr::filter(! name %in% c("twitter_users", "online_users")) %>%
+    dplyr::filter(! (type=="reader"&name=="count")) %>%
+    dplyr::mutate(name = stringr::str_replace(name, "twitter_tweets", "twitter")) %>%
 
-  df_alm_time <- df_out %>%
+    dplyr::filter(is.na(name)==F) %>%
+    dplyr::group_by(id) %>%
+    dplyr::mutate(total = sum(value))  %>% dplyr::mutate(prop = value / total) %>%
+    dplyr::ungroup()
+
+
+
+  # Output --------------
+  df_out <-
+
+
+
+    temporal <- df %>%
     dplyr::select(id, doi, starts_with("alm_score"), starts_with("date_")) %>%
     dplyr::mutate(alm_score_0d = 0) %>%
     dplyr::mutate(add2now = (date_update-date_added) %>% as.numeric()) %>%
@@ -183,30 +137,16 @@ impact_altmetric <- function(id_list){
     dplyr::select(-add2now, -date_publish) %>%
     dplyr::arrange(doi, alm_time)
 
-  df_alm_source <- df_out %>%
-    dplyr::select(id, doi, starts_with("n_engage_")) %>%
-    dplyr::filter(is.na(doi)==F) %>%
-    tidyr::pivot_longer(cols = starts_with('n_engage_')&-ends_with('_all'),
-                        names_to = "source", values_to = "n") %>%
-    dplyr::mutate(source = stringr::str_replace(source, "n_engage_", "")) %>%
-    dplyr::filter(! source %in% c("twitter_accounts")) %>%
-    dplyr::mutate(source = stringr::str_replace(source, "twitter_posts", "twitter")) %>%
-    dplyr::mutate(source = factor(source, levels = c(dplyr::group_by(., source)   %>%
-                                                       dplyr::summarise(sum = sum(n)) %>%
-                                                       dplyr::arrange(-sum) %>%
-                                                       dplyr::filter(sum>0) %>%
-                                                       dplyr::pull(source)))) %>%
-    dplyr::filter(is.na(source)==F) %>%
-    dplyr::group_by(id) %>%
-    dplyr::mutate(total = sum(n))  %>% dplyr::mutate(prop = n / total) %>%
-    dplyr::ungroup()
-
-  df_alm_rank <- df_out %>%
+  rank <- df %>%
     dplyr::select(id, doi, journal,alm_score_now,
                   starts_with("alm_all_"),
                   starts_with("alm_journal_all_"),
                   starts_with("alm_3m_"),
                   starts_with("alm_journal_3m_")) %>%
+    dplyr::mutate(alm_all_prop = 1-((alm_all_rank-1)/alm_all_n),
+                  alm_3m_prop = 1-((alm_3m_rank-1)/alm_3m_n),
+                  alm_journal_all_prop = 1-((alm_journal_all_rank-1)/alm_journal_all_n),
+                  alm_journal_3m_prop = 1-((alm_journal_3m_rank-1)/alm_journal_3m_n)) %>%
     tidyr::pivot_longer(cols = -c(id, doi, journal, alm_score_now), names_to = "alm_category") %>%
 
     dplyr::mutate(alm_category = gsub("alm_", "", alm_category),
@@ -227,9 +167,7 @@ impact_altmetric <- function(id_list){
     dplyr::filter(is.na(mean)==F) %>%
     dplyr::rename("alm_score" = alm_score_now)
 
-    df_out <- id_class %>%
-      dplyr::left_join(df_out, by = "id")
+  out <- list("df" = df %>%dplyr::select(id, "altmetric" = alm_score_now), "temporal" = temporal, "rank" = rank, "source" = sources)
 
-  out <- list("df" = df_out, "temporal" = df_alm_time, "rank" = df_alm_rank, "source" = df_alm_source)
 
   return(out)}
--- R/score_alm.R
+++ R/score_alm.R
@@ -2,7 +2,7 @@
 # Documentation
 #' Extract overall almetric score for a publication
 #' @description Extract overall almetric score for a publication
-#' @param id Vector of unique Digital Object Identifiers (DOI) and/or PMID
+#' @param id_list Vector of unique Digital Object Identifiers (DOI) and/or PMID
 #' @return Vector of overall almetric scores (or NA if not available)
 #' @import purrr
 #' @import tibble
@@ -11,21 +11,30 @@
 #' @export
 
 # Function-------------------------------
-score_alm <- function(id){
+score_alm <- function(id_list){
   require(dplyr);require(tibble);require(purrr); require(rAltmetric)
 
-  id_class <- id %>%
-    tibble::enframe(name = "n", value = "id") %>%
-    # https://www.crossref.org/blog/dois-and-matching-regular-expressions/
-    dplyr::mutate(id_type = suppressWarnings(dplyr::if_else(grepl("^10\\.\\d{4,9}/", id)==T,
-                                                            "doi",
-                                                            if_else(nchar(id)==8&is.numeric(as.numeric(id))==T,
-                                                                    "pmid", "invalid"))))
+  # Extract data
+  id_class <- suppressWarnings(as.character(id_list) %>%
+                                 tibble::enframe(name = "n", value = "id") %>%
+                                 # https://www.crossref.org/blog/dois-and-matching-regular-expressions/
+                                 dplyr::mutate(id_type = dplyr::case_when(stringr::str_detect(id, "^10\\.\\d{4,9}/")==T ~  "doi",
+                                                                          nchar(id)==8&is.numeric(as.numeric(id))==T ~ "pmid",
+                                                                          TRUE ~ "invalid")))
+ score <- id_class %>%
+    dplyr::filter(id_type %in% c("pmid", "doi")) %>%
+    dplyr::mutate(request = paste0("https://api.altmetric.com/v1/", id_type, "/",id)) %>%
+    dplyr::mutate(response = purrr::map(request, function(x){httr::GET(x)}),
+                  response404 = purrr::map_chr(response, function(x){httr::status_code(x) == 404})) %>%
+    dplyr::filter(response404==FALSE) %>%
+    dplyr::select(-request, -response404,-n,-id_type) %>%
+    dplyr::mutate(purrr::map_df(response, function(x){
+      jsonlite::fromJSON(httr::content(x, as = "text"), flatten = TRUE) %>%
+        rlist::list.flatten() %>%
+        tibble::enframe() %>%
+        tidyr::pivot_wider(everything())})) %>%
+    dplyr::select(-response) %>%
+    dplyr::right_join(id_class %>% select(id), by = "id") %>%
+    pull(score) %>% unlist()
 
-  score <- id_class %>%
-    dplyr::mutate(altmetric = ifelse(id_type == "doi", purrr::map_chr(id,
-                                                                 function(x){tryCatch(rAltmetric::altmetric_data(rAltmetric::altmetrics(doi = x))$score, error=function(e){NA})}), NA),
-                  altmetric = ifelse(id_type == "pmid", purrr::map_chr(id,
-                                                                     function(x){tryCatch(rAltmetric::altmetric_data(rAltmetric::altmetrics(pmid = x))$score, error=function(e){NA})}), altmetric)) %>%
-    dplyr::pull(altmetric)
     return(score)}
